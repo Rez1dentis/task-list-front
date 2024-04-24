5720f909196943ac6df86ed26ba8344a38ff55fe
Jean <jean.kosenko@gmail.com>
2024-04-24 17:28:43 +0300

    font added

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,3 +1,9 @@
+import './Main.scss';
+
 export const Main = (): JSX.Element => {
-  return <div>dfghsdfgh</div>;
+  return (
+    <div className="container">
+      <h1>Task List</h1>
+    </div>
+  );
 };
