795f22af44c319fb83f2f08d0490377293b2219e
Jean <jean.kosenko@gmail.com>
2024-04-26 12:21:26 +0300

    add task done

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,19 +1,10 @@
 import classes from './Main.module.scss';
-import { HiOutlinePlus } from 'react-icons/hi';
 import { TaskApp } from './TaskApp/TaskApp';
 
 export const Main = (): JSX.Element => {
   return (
     <div className={classes.mainContainer}>
-      <div className={classes.header}>
-        <div className={classes.title}>Task List</div>
-        <div className={classes.icon}>
-          <HiOutlinePlus style={{ width: 45, height: 45, color: 'white', cursor: 'pointer' }} />
-        </div>
-      </div>
-      <div className={classes.tasksContainer}>
-        <TaskApp />
-      </div>
+      <TaskApp />
       <div className={classes.footer} />
     </div>
   );
